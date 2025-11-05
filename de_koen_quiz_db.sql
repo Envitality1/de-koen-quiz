6ac6be7bea39c66a4cf460a8b92f31efecb5d18a
tamirboesjes@outlook.com <tamirboesjes@outlook.com>
2025-11-05 15:07:26 +0100

    remove question daily sync

--- de_koen_quiz_db.sql
+++ de_koen_quiz_db.sql
@@ -1,1 +1,17 @@
-select * from answers
+DROP TABLE IF EXISTS answers CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  question TEXT NOT NULL,
+  choices TEXT, -- optional, for multiple choice
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY,
+  user_name TEXT,
+  answer TEXT,
+  question_id INT REFERENCES questions(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
